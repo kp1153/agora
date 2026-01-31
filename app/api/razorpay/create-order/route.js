a8bc5db084de7e53af3c69b6e9978810c8e1ac88
kp1153 <kamta_tiwari@yahoo.com>
2026-01-31 16:38:19 +0530

    force deploy

--- app/api/razorpay/create-order/route.js
+++ app/api/razorpay/create-order/route.js
@@ -1,13 +1,13 @@
 import Razorpay from 'razorpay';
 import { NextResponse } from 'next/server';
 
-const razorpay = new Razorpay({
-  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
-
 export async function POST(req) {
   try {
+    const razorpay = new Razorpay({
+      key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+
     const { amount, currency, customerDetails, items } = await req.json();
 
     const options = {
